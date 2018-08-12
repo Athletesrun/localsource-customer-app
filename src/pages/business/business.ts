8adaee909999ff3ca03cfd5b232936dcc3fa549b
Joseph <jshap17@gmail.com>
2018-08-11 20:46:02 -0600

    Got + button working properly.

--- src/pages/business/business.ts
+++ src/pages/business/business.ts
@@ -15,6 +15,56 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BusinessPage {
 
+  customerServiceCount = 22;
+  customerServiceShouldInc = true;
+  customerServiceIncrement(event){
+    if(this.customerServiceShouldInc){
+      this.customerServiceCount++;
+      event.target.classList.remove("b-class");
+      event.target.classList.add("a-class");
+    } 
+    else {
+      this.customerServiceCount--;
+      event.target.classList.remove("a-class");
+      event.target.classList.add("b-class");
+    } 
+
+    this.customerServiceShouldInc = !this.customerServiceShouldInc;
+  }
+
+  qualityCount = 13;
+  qualityShouldInc = true;
+  qualityIncrement(event){
+    if(this.qualityShouldInc){
+      this.qualityCount++;
+      event.target.classList.remove("b-class");
+      event.target.classList.add("a-class");
+    } 
+    else {
+      this.qualityCount--;
+      event.target.classList.remove("a-class");
+      event.target.classList.add("b-class");
+    } 
+
+    this.qualityShouldInc = !this.qualityShouldInc;
+  }
+
+  pricingCount = 9;
+  pricingShouldInc = true;
+  pricingIncrement(event){
+    if(this.pricingShouldInc){
+      this.pricingCount++;
+      event.target.classList.remove("b-class");
+      event.target.classList.add("a-class");
+    } 
+    else {
+      this.pricingCount--;
+      event.target.classList.remove("a-class");
+      event.target.classList.add("b-class");
+    } 
+
+    this.pricingShouldInc = !this.pricingShouldInc;
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
